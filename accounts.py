ef7e59c04fb062fd0a41e033a5ba0f5ca6d2ed6e
conniewambui <consolatawambuimacharia@gmail.com>
2020-07-26 17:33:34 +0300

    bankaccount

--- accounts.py
+++ accounts.py
@@ -1,57 +1,121 @@
+from datetime import datetime
 class BankAccount:
+        
     def __init__(self, bank, phone_number, first_name, last_name):
-        self.first_name= first_name
-        self.last_name= last_name
-        self.balance = 0
         self.bank = bank
         self.phone_number = phone_number
-        self.loan_balance = 0
+        self.first_name = first_name
+        self.last_name = last_name
+        loan = {}
+        self.balance = 0
+        self.deposits = []
+        self.withdrawals = []
+        
+
+    def getCurrentTime(self):
+        now = datetime.now()
+        time_formatted = now.strftime("%b %d %Y, %H;%M:%S")  
+        return time_formatted  
     
         
     def account_name(self):
-        name = "{} account for  {} {}".format(self.bank, self.first_name, 
-        self.last_name, self.deposit)
-        
+        time = self.getCurrentTime()
+        name = "{} account for  {} {} at ".format(self.bank, self.first_name,self.last_name, time)
+
         return name
         
+   
     def deposit(self, amount):
+        
 
-        self.balance += amount 
-        print("You have deposited {} into your account ".format(amount))
-    
-    
-    def withdraw(self, amount): 
 
-        if self.balance>=amount: 
-            self.balance-=amount
-            print("\n You Withdrew:", amount) 
+        amount = input("Enter the amount you want to deposit : ")
+        try:
+            amount = int(amount)
+        except:
+            print("Invalid input, only numbers are allowed.") 
+            return
+        if amount <= 0:
+            print("You can not deposit zero or a negative amount")
         else:
-            print("\n Insufficient balance  ") 
+            self.balance += amount
+            self.deposits.append(amount) 
+            time = self.getCurrentTime()
+            
+            print("You have deposited {} to {} on {} ".format(amount, self.account_name(),time)) 
 
-    
+      
+    def withdraw(self, amount):
+        amount = input("Enter the amount you want to withdraw : ")
+        try:
+            amount = int(amount)
+        except:
+            print("Invalid input, only numbers are allowed.") 
+            return   
+          
+        if amount <= 0:
+              print("You cannot withdraw a zero or a negative amount")
+
+        else:
+            self.balance -= amount
+            self.withdrawals.append(amount)
+            time = self.getCurrentTime()
+            print("You have withdrawn {} from {} at {}".format(amount, self.account_name(),time))
+
+ 
     def  get_balance(self):
+        time = self.getCurrentTime()
+        return "The balance for {} is {} at {}".format(self.account_name(),self.balance, time)
+     
+    def show_deposit_statement(self):
+        for deposit in self.deposits:
+            time = self.getCurrentTime()
 
-        return "The balance for {} is {}".format(self.account_name(),self.loan)
+            print("Dear customer your deposit statement is {} as at {}.".format(deposit,time))
+       
 
-    def loan_balance(self,amount):
-        if loan_balance < 0: 
-            self.loan_balance += amount
+    def show_withdrawal_statement(self):
+        for withdrawal in self.withdrawals:
+            time = self.getCurrentTime()
+            print("Dear customer your withdrawal statement is {} as at {}.".format(withdrawal,time))
+
+
+
+    def request_loan(self,amount): 
+        amount = input("Enter the amount of loan you want : ")
+        try:
+            amount = int(amount)
+        except:
+            print("!!!Invalid input, only numbers are allowed.") 
+            return
+        if amount <= 0:
+            print("Dear customer, you cannot request a loan of zero or negative amount.")
+
+        else: 
+            self.loan = amount 
+            time = self.getCurrentTime() 
+            print("Dear customer, your account has been debited with a loan of {} on {}".format(amount,time))   
+
+    def repay_loan(self,amount):
+        amount = input("Enter the amount of loan you want to REPAY : ")
+        try:
+            amount = int(amount)
+        except:
+            print("!!!Invalid input, only numbers are allowed.") 
+            return
+        if amount <= 0:
+            print("Dear Customer you cannot repay you loan with a zero or a negative amount.")
+        elif self.loan == 0:
+            print("Dear Customer you do not have a loan at the moment.")
+
+        elif amount > self.loan :
+            time = self.getCurrentTime()
+            print("Dear Customer, Your loan is {} , please enter an amount that is less or equal to your loan {}." .format(self.loan,time))
 
         else:
-          print("Sorry you already have a loan balance.")
-
-
-
-
-    def repay(self,amount):
-        if self.loan_balance == 0:
-            print("You have no loan balance")
-        elif amount>self.loan_balance:
-            self.loan_balance=0
-
-        elif amount<=self.loan_balance:
-            self.loan_balance-=amount
-        return
+            time = self.getCurrentTime()
+            self.loan -= amount
+            print("Dear Customer, you have repaid your loan with {} , your loan balance is {} on {}".format(amount,self.loan,time))
 
             
                   
